82ad7ccf66a182ee6927a85ea383c2e4ce50c404
Mark Robidoux <robidouxmark@gmail.com>
2017-09-28 00:55:55 -0400

    Adding basic command line interface, tests, and first solution for 2015.

---
+++ AdventOfCode/Solutions/Challenges2015/NotQuiteLisp.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode.Solutions.Challenges2015
+{
+    public class NotQuiteLisp
+    {
+        public static int CalculateFinalFloor(string input)
+        {
+            var floor = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    floor++;
+                }
+                else if (input[i] == ')')
+                {
+                    floor--;
+                }
+            }
+
+            return floor;
+        }
+
+        public static int CharacterPositionOfFirstBasementLanding(string input)
+        {
+            var position = 0;
+            var floor = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    floor++;
+                }
+                else if (input[i] == ')')
+                {
+                    floor--;
+                }
+
+                if (floor == -1)
+                {
+                    position = i + 1;
+                    break;
+                }
+            }
+
+            return position;
+        }
+    }
+}
---
+++ AdventOfCode/Tests/Challenges2015/NotQuiteLispTests.cs
@@ -0,0 +1,78 @@
+﻿using AdventOfCode.Solutions.Challenges2015;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests
+{
+    [TestFixture]
+    public class NotQuiteLispTests
+    {
+        [TestCase("(", 1, TestName = "OneLeftParenReturnsOne")]
+        [TestCase("((", 2, TestName = "TwoLeftParenReturnsTwo")]
+        [TestCase("(((", 3, TestName = "ThreeLeftParenReturnsThree")]
+        [TestCase("((((((((((", 10, TestName = "TenLeftParenReturnsTen")]
+        public void LeftParenIncreasesFinalFloor(string input, int expectedOutput)
+        {
+            var result = NotQuiteLisp.CalculateFinalFloor(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestCase(")", -1, TestName = "OneRightParenReturnsNegativeOne")]
+        [TestCase("))", -2, TestName = "TwoRightParenReturnsNegativeTwo")]
+        [TestCase(")))", -3, TestName = "ThreeRightParenReturnsNegativeThree")]
+        [TestCase("))))))))))", -10, TestName = "TenRightParenReturnsNegativeTen")]
+        public void RightParenDescreasesFinalFloor(string input, int expectedOutput)
+        {
+            var result = NotQuiteLisp.CalculateFinalFloor(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestCase("))(", -1, TestName = "OneMoreRightParenReturnsNegativeOne")]
+        [TestCase("))()()())(", -2, TestName = "TwoMoreRightParensReturnsNegativeTwo")]
+        [TestCase("(()()(())", 1, TestName = "OneMoreLeftParenReturnsOne")]
+        [TestCase("(((())))((", 2, TestName = "TwoMoreLeftParensReturnsTwo")]
+        public void UnequalMixedParensReturnsNonZeroFinalFloor(string input, int expectedOutput)
+        {
+            var result = NotQuiteLisp.CalculateFinalFloor(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void EqualMixedParensReturnsZeroFloor()
+        {
+            var result = NotQuiteLisp.CalculateFinalFloor("))((");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void EmptyStringReturnsZeroFloor()
+        {
+            var result = NotQuiteLisp.CalculateFinalFloor("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestCase(")", 1, TestName = "SingleRightParenGetsToBasementOnCharacterOne")]
+        [TestCase(")()())))(((()(()))()()", 1, TestName = "LeadingRightParenGetsToBasementOnCharacterOne")]
+        [TestCase("(()))", 5, TestName = "CalculateFirstLandingReturnsCharacterPositionOfFirstNegativeFloor")]
+        public void CharacterPositionOfFirstBasementLandingReturnsCorrectCharacterPosition(string input, int expectedOutput)
+        {
+            var result = NotQuiteLisp.CharacterPositionOfFirstBasementLanding(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestCase("(", TestName = "SingleLeftParenRetrunsZero")]
+        [TestCase("((((((", TestName = "MultipleLeftParenReturnsZero")]
+        [TestCase("(((((()))", TestName = "NeverReachingBasementReturnsZero")]
+        public void CharacterPositionOfFirstBasementLandingReturnsZeroWhenNeverGettingToBasement(string input)
+        {
+            var result = NotQuiteLisp.CharacterPositionOfFirstBasementLanding(input);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
