e7f88f747396c19214decd6bc50ef147067c6ad6
Mark Robidoux <robidouxmark@gmail.com>
2017-09-29 22:51:59 -0400

    Adding solution for Day Two part 1

---
+++ AdventOfCode/Solutions/Challenges2015/ToldNoMath.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode.Solutions.Challenges2015
+{
+    public class ToldNoMath
+    {
+        public static int CalculateTotalSurfaceAreaWithSlack(int[,] boxes)
+        {
+            var totalSurfaceArea = 0;
+
+            for (var row = 0; row < boxes.Length / 3; row++)
+            {
+                ArrangeBoxOnSmallestSide(boxes, row);
+
+                totalSurfaceArea += 2 * (boxes[row, 0] * boxes[row, 1] + boxes[row, 0] * boxes[row, 2] + boxes[row, 1] * boxes[row, 2]) + boxes[row, 0] * boxes[row, 1];
+            }
+
+            return totalSurfaceArea;
+        }
+
+        private static void ArrangeBoxOnSmallestSide(int[,] boxes, int boxNumber)
+        {
+            if (boxes[boxNumber, 0] < boxes[boxNumber, 1])
+            {
+                //0 < 1
+                if (boxes[boxNumber, 1] > boxes[boxNumber, 2])
+                {
+                    //2 < 1
+                    var temp = boxes[boxNumber, 1];
+                    boxes[boxNumber, 1] = boxes[boxNumber, 2];
+                    boxes[boxNumber, 2] = temp;
+                }
+            }
+            else
+            {
+                //1 <= 0
+                if (boxes[boxNumber, 0] > boxes[boxNumber, 2])
+                {
+                    //2 < 0
+                    var temp = boxes[boxNumber, 0];
+                    boxes[boxNumber, 0] = boxes[boxNumber, 2];
+                    boxes[boxNumber, 2] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Tests/Challenges2015/ToldNoMathTests.cs
@@ -0,0 +1,81 @@
+﻿using AdventOfCode.Solutions.Challenges2015;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Challenges2015
+{
+    [TestFixture]
+    public class ToldNoMathTests
+    {
+        static object[] singleBoxTestSource =
+        {
+            new object[] { new int[,] { { 1, 1, 1 } }, 7 },
+            new object[] { new int[,] { { 1, 2, 1 } }, 11 },
+            new object[] { new int[,] { { 2, 3, 4 } }, 58 },
+            new object[] { new int[,] { { 10, 10, 11 } }, 740 },
+            new object[] { new int[,] { { 7, 3, 4 } }, 134 },
+            new object[] { new int[,] { { 3, 1, 5 } }, 49 },
+            new object[] { new int[,] { { 5, 6, 7 } }, 244 }
+        };
+
+        [Test, TestCaseSource("singleBoxTestSource")]
+        public void SingleBoxReturnsCorrectValue(int[,] input, int expectedValue)
+        {
+            var result = ToldNoMath.CalculateTotalSurfaceAreaWithSlack(input);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        static object[] orientationTestSource =
+        {
+            new object[] { new int[,] { { 2, 5, 7 } }, 128 },
+            new object[] { new int[,] { { 2, 7, 5 } }, 128 },
+            new object[] { new int[,] { { 5, 2, 7 } }, 128 },
+            new object[] { new int[,] { { 5, 7, 2 } }, 128 },
+            new object[] { new int[,] { { 7, 2, 5 } }, 128 },
+            new object[] { new int[,] { { 7, 5, 2 } }, 128 }
+        };
+
+        [Test, TestCaseSource("orientationTestSource")]
+        public void OrientationDoesNotMatter(int[,] input, int expectedValue)
+        {
+            var result = ToldNoMath.CalculateTotalSurfaceAreaWithSlack(input);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        static object[] multipleBoxTestSource =
+        {       
+            new object[] { new int[,] { { 1, 1, 1 }, { 2, 3, 4 } }, 65 },
+            new object[] { new int[,] { { 1, 2, 1 }, { 7, 3, 4 } }, 145 },
+            new object[] { new int[,] { { 2, 3, 4 }, { 10, 10, 11 } }, 798 },
+            new object[] { new int[,] { { 10, 10, 11 }, { 3, 1, 5 } }, 789 },
+            new object[] { new int[,] { { 7, 3, 4 }, { 1, 1, 1 } } , 141 },
+            new object[] { new int[,] { { 3, 1, 5 }, { 5, 6, 7 } }, 293 },
+            new object[] { new int[,] { { 5, 6, 7 }, { 1, 2, 1 } } , 255 }
+        };
+
+        [Test, TestCaseSource("multipleBoxTestSource")]
+        public void MultipleBoxesReturnsCorrectValue(int[,] input, int expectedValue)
+        {
+            var result = ToldNoMath.CalculateTotalSurfaceAreaWithSlack(input);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        static object[] orderTestSource =
+        {
+            new object[] { new int[,] { { 1, 1, 1 }, { 1, 2, 1 }, { 2, 3, 4 }, { 10, 10, 11 }, { 7, 3, 4 }, { 3, 1, 5 }, { 5, 6, 7 } }, 1243 },
+            new object[] { new int[,] { { 10, 10, 11 }, { 1, 1, 1 }, { 1, 2, 1 }, { 7, 3, 4 }, { 2, 3, 4 }, { 3, 1, 5 }, { 5, 6, 7 } }, 1243 },
+            new object[] { new int[,] { { 1, 1, 1 }, { 2, 3, 4 }, { 10, 10, 11 }, { 1, 2, 1 }, { 7, 3, 4 }, { 3, 1, 5 }, { 5, 6, 7 } }, 1243 },
+            new object[] { new int[,] { { 1, 2, 1 }, { 2, 3, 4 }, { 5, 6, 7 }, { 10, 10, 11 }, { 1, 1, 1 }, { 7, 3, 4 }, { 3, 1, 5 } }, 1243 }
+        };                              
+
+        [Test, TestCaseSource("orderTestSource")]
+        public void OrderDoesNotMatter(int[,] input, int expectedValue)
+        {
+            var result = ToldNoMath.CalculateTotalSurfaceAreaWithSlack(input);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
